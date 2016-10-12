6a2016df889facbca221e8acc41498d2332a0c59
kaja <kaja@kajas-MacBook.fritz.box>
2016-10-12 23:22:16 +0200

    adding jQuery to the message box (.form-control)

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+// twitter button footer
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+
+
+$(document).ready(function(){
+  // your code goes here
+  //alert("Document is ready!")
+  $(".form-control").css("border", "2px solid red");
+  // Smooth scrolling
+  var $root = $('html, body');
+  $('.navbar-nav a').click(function() {
+      var href = $.attr(this, 'href');
+      $root.animate({
+          scrollTop: $(href).offset().top
+      }, 500, function () {
+          window.location.hash = href;
+      });
+      return false;
+  });
+
+  // Stellar
+  $.stellar();
+  $(window).stellar();
+    // or:
+  $('#main').stellar()
+
+  // Tooltips
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+  $(function () {
+    $('#item1').tooltip();
+  });
+
+  // EVENT LISTENER
+  $("#button").on('click', function() {
+  // now we're going to work in here
+  // more code here
+  $('#button').on('click', function() {
+  var comment = $('.form-control').val();
+  $('#visible-comment').html(comment);
+  console.log(comment)
+  return false;
+	});
+
+  console.log('clicked');
+  return false;
+  //no more code here
+	});
+
+  //textarea background
+
+});
